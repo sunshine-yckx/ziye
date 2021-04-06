b69012e8cf4fffe8b4bd1bea214c655bd852f261
sunshine-yckx <68091104+sunshine-yckx@users.noreply.github.com>
2021-04-06 22:30:41 +0800

    Update ysm3.js

--- Task/ysm3.js
+++ Task/ysm3.js
@@ -130,7 +130,7 @@ if ($.isNode()) {
 	
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
- else {ysmurlArr.push($.getdata('ysmurl'))
+} else {ysmurlArr.push($.getdata('ysmurl'))
     ysmhdArr.push($.getdata('ysmhd'))
     ysmbodyArr.push($.getdata('ysmbody'))
     ysm2bodyArr.push($.getdata('ysm2body'))
